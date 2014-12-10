c9514538ac41a20bb0ed57d25971427f842a77b7
Andrey Smetanin <irqlevel@gmail.com>
2014-12-10 14:42:04 +0300

    string permutations

---
+++ java/src/algolib/StringPerms.java
@@ -0,0 +1,36 @@
+package algolib;
+
+import java.util.ArrayList;
+
+public class StringPerms {
+
+	public static ArrayList<String> getAllPerms(String s)
+	{
+		ArrayList<String> perms = new ArrayList<String>();
+		if (s.length() == 0) {
+			perms.add("");
+			return perms;
+		}
+	
+		char first = s.charAt(0);
+		String remainder = s.substring(1);
+			
+		ArrayList<String> remPerms = getAllPerms(remainder);
+		for (int i = 0; i < remainder.length() + 1; i++) {
+			for (String perm : remPerms) {
+				String p1 = perm.substring(0, i);
+				String p2 = perm.substring(i);
+				perms.add(p1 + first + p2);
+			}
+		}
+
+		return perms;	
+	}
+	
+	public static void main(String args[]) {
+		ArrayList<String> perms = getAllPerms("prov");
+		for (String s : perms) {
+			System.out.println(s);
+		}
+	}
+}
