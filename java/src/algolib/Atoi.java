a1930503509a08f0a9f182111658fbb4d2cf533d
Andrey Smetanin <irqlevel@gmail.com>
2014-12-20 21:32:38 +0300

    atoi check base

--- java/src/algolib/Atoi.java
+++ java/src/algolib/Atoi.java
@@ -5,6 +5,9 @@ public class Atoi {
 	public static int atoi(char[] s, int base) throws Exception {
 		int result = 0;
 		
+		if (base < 2 || base > 10)
+			throw new Exception("unsupported base");
+		
 		for (int i = 0; i < s.length; i++) {
 			if (s[i] == '\0')
 				break;
@@ -20,6 +23,7 @@ public class Atoi {
 	}
 	
 	public static void main(String args[]) throws Exception {
-		System.out.println(atoi(new String("10000").toCharArray(), 2));
+		System.out.println(atoi(new String("1010").toCharArray(), 2));
+		System.out.println(atoi(new String("0395").toCharArray(), 10));
 	}
 }
